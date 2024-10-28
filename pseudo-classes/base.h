cac474b6a1c9112b511eeeb83c40ae1b73aadce2
anghievisck <pedro.luis.angh@gmail.com>
2024-10-28 15:57:21 -0300

    Changing headers

--- pseudo-classes/base.h
+++ pseudo-classes/base.h
@@ -1,33 +1,27 @@
-#ifndef USER_H
-#define USER_H
-
-#include "user.h"
-
-typedef struct user User;
-typedef struct msg Msg;
-typedef struct group Group;
-typedef struct node Node;
-
 typedef union{
-    User *user;
-    Msg  *msg;
-    Group *grp;
+    struct user *user;
+    struct msg *msg;
+    struct group *grp;
 }Dado;
+
 typedef struct node{
     Dado *info;
-    Node *next, *previous;
+    struct node *next, *previous;
 }Node;
+
 typedef struct list{
     int total;
     Node *start, *end;
 }List;
 
 void Create(List*);
+
 void Push(List*, Dado*);
+
 Dado* Pop(List*, int *);
 Dado* Out(List*, int *);
+
 Dado* Top(List*, int *);
 Dado* Bottom(List*, int *);
-void TurnFirst(List *l, Node *node);
-#endif 
 
+void TurnFirst(List *l, Node *node);
